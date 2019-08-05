4c2e68c7b3a27a85905ef3a91660e434561a85b2
puppee <pupp3@icloud.com>
2019-08-05 16:41:25 +0300

    Revert Geo, remove .app

--- src/App.js
+++ src/App.js
@@ -2,19 +2,11 @@ import React, { Component } from 'react';
 import './App.css';
 import { Map, GoogleApiWrapper } from 'google-maps-react';
 import apiConfig from './apikeys.js';
-import { geolocated } from "react-geolocated";
 
 const mapStyles = {
   width: '100%',
   height: '100%',
 };
-
-function App() {
-  return (
-    <div className="App">
-    </div>
-  );
-}
 
 export class MapContainer extends Component {
   render() {
