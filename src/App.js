54a7b6166146d2c8aaaa65c0b5e1111770ee3adb
puppee <pupp3@icloud.com>
2019-08-07 18:16:44 +0300

    Daily0708

--- src/App.js
+++ src/App.js
@@ -1,6 +1,22 @@
 import React from 'react';
+import ReactDOM from "react-dom";
 import './App.css';
 import { withScriptjs, withGoogleMap, GoogleMap } from "react-google-maps"
+// import PlacesAutocomplete, { geocodeByAddress, getLatLng, } from 'react-places-autocomplete';
+import { geolocated } from "react-geolocated";
+
+class LocateMe extends React.Component {
+	render() {
+		return !this.props.isGeoLocationAvailable ? (
+		console.log('GeoLoc ei saatavilla')
+		) : !this.props.isGeoLocationEnabled ? (
+		console.log('GeoLoc ei sallittu')
+		) : this.props.coords ? (
+		console.log(this.props.latitude, this.props.longitude)
+		) : (console.log('Lataa sijaintia')
+		);
+	};
+};
 
 const defaultMapOptions = {
   fullscreenControl: false,
@@ -12,8 +28,8 @@ const defaultMapOptions = {
 function Map() {
 	return (
 		<GoogleMap
-			defaultZoom={10}
-			defaultCenter={{ lat: -34.397, lng: 150.644 }}
+			defaultZoom={12}
+			defaultCenter={{ lat: 60.192059, lng: 24.945831 }}
 			defaultOptions={defaultMapOptions}
 		/>
 		);
@@ -25,7 +41,7 @@ export default function App() {
 	return (
 		<div style={{width: '100vw', height: '100vh'}}>
 			<MainScreenMap
-  				googleMapURL={'https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=AIzaSyAIcTSkJY2iEQkg4Llb6SSuMngPVbQShYQ'}
+  				googleMapURL={'https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing&key=AIzaSyAIcTSkJY2iEQkg4Llb6SSuMngPVbQShYQ'}
   				loadingElement={<div style={{ height: `100%` }} />}
   				containerElement={<div style={{ height: `100%` }} />}
   				mapElement={<div style={{ height: `100%` }} />}
